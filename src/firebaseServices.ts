79816d9ba73b7dcfab5197b288111e1053410db9
a-cola <cola.andrea.2000@gmail.com>
2024-04-26 16:56:23 +0200

    feat: add genres query

--- src/firebaseServices.ts
+++ src/firebaseServices.ts
@@ -1,4 +1,4 @@
-import { collection, doc, getDoc, getDocs, limit, orderBy, query} from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, limit, orderBy, query, where} from "firebase/firestore";
 import { db } from "./main";
 
 export interface GameData {
@@ -19,9 +19,11 @@ export interface Game extends GameData {
     id: string;
 }
 
-export const getGameList = async () => {
+export const getGameList = async (genre?:string) => {
     const gameCollection = collection(db, "videogames");
-    const gameQuery = query(gameCollection, orderBy("title", "asc"));
+    const gameQuery = genre===undefined
+        ?query(gameCollection, orderBy("title", "asc"))
+        :query(gameCollection, where("genres", "array-contains", genre))
     const gameSnapshot = await getDocs(gameQuery);
     const gameList:Game[] = gameSnapshot.docs.map(doc => ({id:doc.id, ...(doc.data() as GameData)}));
     return gameList;
